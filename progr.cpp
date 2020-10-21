fa026530dcac758abdf9a93fabdb8fba38267d4e
Hyperboloider <kknyaz033@gmail.com>
2020-10-21 14:44:08 +0300

    done

--- progr.cpp
+++ progr.cpp
@@ -1,16 +1,17 @@
+//13. Обчислити n-ий член арифметичної 
 #include <iostream>
 
 int main()
 {   
-    float firstMember, difference, result;              //визначимо тип вхідних змінних та змінної результату
-    int number;                                         //визничамо тип 
+    float firstMember, difference, result;                                         //визначимо тип вхідних змінних та змінної результату
+    int number;                                                                    //визничамо тип змінної номеру шуканого члена
     std::cout << "перший член прогресії (число): ";
-    std::cin >> firstMember;                            //введення першого члену прогресії
+    std::cin >> firstMember;                                                       //введення першого члену прогресії
     std::cout << "різниця прогресії (число): ";
-    std::cin >> difference;                             //введення різниці прогресії
+    std::cin >> difference;                                                        //введення різниці прогресії
     std::cout << "номер шуканого члену (ціле число, більше за нуль): ";
-    std::cin >> number;                                 //введення номеру шуканого члену
-    result = firstMember + difference * (number - 1);   //обчислення шуканого члену
-    std::cout << "шукане: " << result << std::endl;     //виведення шуканого
+    std::cin >> number;                                                            //введення номеру шуканого члену
+    result = firstMember + difference * (number - 1);                              //обчислення шуканого члену
+    std::cout << "шукане: " << result << std::endl;                                //виведення шуканого
     return 0;
 }
